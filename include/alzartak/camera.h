a85b44f73e5c514a4ce1ad0f7749ed580232a911
sopiro <sopiro.ldh@gmail.com>
2024-08-21 02:45:06 +0900

    Update sample Add run script

--- include/alzartak/camera.h
+++ include/alzartak/camera.h
@@ -5,7 +5,28 @@
 namespace alzartak
 {
 
-struct Camera
+struct Camera2D
+{
+    Point2 position{ 0 };
+    float rotation{ 0 };
+    Point2 scale{ 1 };
+
+    Mat4 GetCameraMatrix() const
+    {
+        // Inverse scale
+        Mat4 m{ Vec4(1 / scale.x, 1 / scale.y, 1, 1) };
+
+        // Inverse rotation
+        m = MulT(Mat4(Quat::FromEuler({ 0, 0, rotation }), Vec3::zero), m);
+
+        // Inverse translation
+        m = m.Translate({ -position.x, -position.y, 0 });
+
+        return m;
+    }
+};
+
+struct Camera3D
 {
     Point3 position{ 0 };
     Point3 rotation{ 0 };
--- test/main.cpp
+++ test/main.cpp
@@ -10,12 +10,40 @@ using namespace alzartak;
 
 Window* window = nullptr;
 Vec4 clear_color{ 190.0f / 255.0f, 220.0f / 255.0f, 230.0f / 255.0f, 1.0f };
+float scale = 100.0f;
+Renderer* renderer;
+Camera2D* camera;
+
+void UpdateProjectionMatrix()
+{
+    Vec2 extents = window->GetWindowSize() / scale;
+    Mat4 proj_matrix = Mat4::Orth(-extents.x / 2.0f, extents.x / 2.0f, -extents.y / 2.0f, extents.y / 2.0f, 0.0f, 1.0f);
+    // Mat4 proj_matrix = Mat4::Perspective(DegToRad(71.0f), 16.0f / 9.0f, 0.0f, 1000.0f);
+
+    renderer->SetProjectionMatrix(proj_matrix);
+}
+
+void Init()
+{
+    window = Window::Init(1280, 720, "alzartak");
+    renderer = new Renderer;
+    camera = new Camera2D;
+
+    UpdateProjectionMatrix();
+    window->SetFramebufferSizeChangeCallback([&](int32 width, int32 height) -> void {
+        glViewport(0, 0, width, height);
+        UpdateProjectionMatrix();
+    });
+}
+
+void Terminate()
+{
+    delete camera;
+    delete renderer;
+}
 
 void MainLoop()
 {
-    static Renderer r;
-    static Camera camera;
-
     window->BeginFrame(clear_color);
     // ImGui::ShowDemoWindow();
 
@@ -24,21 +52,46 @@ void MainLoop()
         std::cout << "wakha" << std::endl;
     }
 
-    constexpr float scale = 100.0f;
-    Vec2 extents = window->GetWindowSize() / scale;
-    WakNotUsed(extents);
-    Mat4 proj_matrix = Mat4::Orth(-extents.x / 2.0f, extents.x / 2.0f, -extents.y / 2.0f, extents.y / 2.0f, 0.0f, 1.0f);
-    // Mat4 proj_matrix = Mat4::Perspective(DegToRad(71.0f), 16.0f / 9.0f, 0.0f, 1000.0f);
+    // Screen grabbing
+    {
+        if (Input::GetMouseScroll().y != 0)
+        {
+            camera->scale *= Input::GetMouseScroll().y < 0 ? 1.1f : 1.0f / 1.1f;
+            camera->scale.x = Clamp(camera->scale.x, 0.1f, max_float);
+            camera->scale.y = Clamp(camera->scale.y, 0.1f, max_float);
+        }
 
-    r.SetProjectionMatrix(proj_matrix);
-    r.SetViewMatrix(camera.GetCameraMatrix());
+        static bool camera_move = false;
+        static Vec2 cursor_start, camera_start;
 
-    r.SetPointSize(5);
-    r.SetLineWidth(3);
-    r.DrawLine({ 0, 0 }, { 1, 1 }, Vec4(1, 0, 1, 1));
-    r.DrawTriangle({ 0, 0 }, { 1, 0 }, { 0.5, 1.0 }, Vec4(0, 1, 0, 1));
-    r.DrawPoint({ -1, -1 }, Vec4(1, 0, 0, 1));
-    r.FlushAll();
+        if (!camera_move && Input::IsMousePressed(GLFW_MOUSE_BUTTON_RIGHT))
+        {
+            camera_move = true;
+            cursor_start = Input::GetMousePosition();
+            camera_start = camera->position;
+        }
+        else if (Input::IsMouseReleased(GLFW_MOUSE_BUTTON_RIGHT))
+        {
+            camera_move = false;
+        }
+
+        if (camera_move)
+        {
+            Vec2 dist = Input::GetMousePosition() - cursor_start;
+            dist.x *= -camera->scale.x / scale;
+            dist.y *= camera->scale.y / scale;
+            camera->position = camera_start + dist;
+        }
+    }
+
+    // Rendering
+    renderer->SetViewMatrix(camera->GetCameraMatrix());
+    renderer->SetPointSize(5);
+    renderer->SetLineWidth(3);
+    renderer->DrawLine({ 0, 0 }, { 1, 1 }, Vec4(1, 0, 1, 1));
+    renderer->DrawTriangle({ 0, 0 }, { 1, 0 }, { 0.5, 1.0 }, Vec4(0, 1, 0, 1));
+    renderer->DrawPoint({ -1, -1 }, Vec4(1, 0, 0, 1));
+    renderer->FlushAll();
 
     window->EndFrame();
 }
@@ -50,7 +103,7 @@ int main()
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
-    window = Window::Init(1280, 720, "alzartak");
+    Init();
 
 #ifdef __EMSCRIPTEN__
     emscripten_set_main_loop(MainLoop, 0, 1);
@@ -60,6 +113,8 @@ int main()
         MainLoop();
     }
 #endif
+
+    Terminate();
 
     return 0;
 }
